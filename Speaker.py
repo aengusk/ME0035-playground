7508cac9255c866adb028d7f461d0e06e4816c92
Anne Hu <anne.hu@tufts.edu>
2024-11-12 22:44:28 -0500

    Update Speaker.py
    Working code for simpleSpeaker, starting to integrate async for normal Speaker object

--- Speaker.py
+++ Speaker.py
@@ -1,29 +1,59 @@
 # speaker.py - By: annehu - Sat Nov 9 2024
 from machine import Pin, PWM
+import uasyncio as asyncio
 import time
 
-class Speaker:
+class Speaker():
+
     def __init__(self, pwm):
-        self.pin = PWM(pwm)
-        self.pin.freq(262)
+        self.pin = pwm
+        self.currNote = 200
+        #self.pin.freq(262)
+        #self.pin.duty_u16(0)
+
+    def setNote(self, note):
+        self.currNote  = note
+
+    async def playNote(self):
+        print("playing note!")
+        self.pin.freq(self.currNote)
+        self.pin.duty_u16(int(65535/8))
+        await asyncio.sleep(0.5)
         self.pin.duty_u16(0)
+
+    async def playNote(self, note):
+        print("playing note!")
+        self.pin.freq(note)
+        self.pin.duty_u16(int(65535/8))
+        await asyncio.sleep(0.5)
+
+
+    async def noteHandler(self):
+        while True:
+            self.playNote()
+            await asyncio.sleep(0.01)
+
+# ------ simpleSpeaker: Base code that doens't use async -------
+class simpleSpeaker:
+    def __init__(self, pwm):
+        self.pin = pwm
 
     def playNote(self,note):
         print("playing note!")
         self.pin.freq(note)
-        self.pin.duty_u16(50000)
+        self.pin.duty_u16(int(65535/8))
         time.sleep(1)
         self.pin.duty_u16(0)
 
 
-
 # Test code
-test = Speaker(Pin('P4',Pin.OUT))
-test.playNote(50)
+# test = Speaker(PWM(Pin('P0', Pin.OUT)))
+# test.playNote(200)
 
-led = PWM(Pin('P7', Pin.OUT))
 
-led.freq(2)
-for i in range(0,63553):
-    led.duty_u16(i)
-    time.sleep(0.01)
+
+# test = PWM(Pin('P0', Pin.OUT))
+# print("starting")
+# test.freq(200)
+# test.duty_u16(int(65535/2))
+# time.sleep(2)
