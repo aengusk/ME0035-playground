4692603130214468fe130edb0e9f495b474dfe9f
Richard Geoghegan <61522274+Richard-Geoghegan@users.noreply.github.com>
2024-11-18 11:32:05 -0500

    Update ESP32-Control.py
    Should all work with ESP Nano communication too

--- AprilTag_music/ESP32-Control.py
+++ AprilTag_music/ESP32-Control.py
@@ -1,6 +1,5 @@
 from machine import Pin, PWM
 import asyncio
-import time
 from BLE_CEEO import Listen
 from networking import Networking
 
@@ -10,7 +9,7 @@ class AprilTagMusicController:
     ADD_PIN   = 19 # D8
     PLAY_PIN  = 20 # D9
     PWM_PIN   = 18 # D10
-    BLUETOOTH_DEVICE_NAME = 'Camera'
+    BLUETOOTH_DEVICE_NAME = 'Camera' # OpenMV Peripheral name
 
     # All possible note frequencies
     Notes = [262, # Index 0: C4
@@ -26,19 +25,21 @@ class AprilTagMusicController:
     def __init__(self):
 
         self.resetButton = Pin(self.RESET_PIN, Pin.IN, Pin.PULL_UP)
-        self.addButton = Pin(self.ADD_PIN, Pin.IN, Pin.PULL_UP)
-        self.playButton = Pin(self.PLAY_PIN, Pin.IN, Pin.PULL_UP)
+        self.addButton   = Pin(self.ADD_PIN, Pin.IN, Pin.PULL_UP)
+        self.playButton  = Pin(self.PLAY_PIN, Pin.IN, Pin.PULL_UP)
 
         self.prev_reset_state = False
-        self.prev_add_state = False
-        self.prev_play_state = False
+        self.prev_add_state   = False
+        self.prev_play_state  = False
 
         self.speaker = PWM(self.PWM_PIN, Pin.OUT)
         self.speaker.duty_u16(int(0))
         self.activeNoteIndex = 0 # Received over bluetooth from OpenMV cam
+        self.activeFoodIndex = None
         self.NotesQueue = [] # Contains the current notes
 
-    
+        self.networking = Networking()
+
     async def checkButtons(self):
         while True:
             current_reset_state = self.resetButton.value()
@@ -96,20 +97,29 @@ class AprilTagMusicController:
                                     self.activeNoteIndex = int(indexes[0].strip())
                                     print(f"Updated activeNoteIndex: {self.activeNoteIndex}")
 
-                                    # TODO set index of food item
+                                    if self.activeFoodIndex != indexes[1]:
+                                        self.activeFoodIndex = indexes[1]
+                                        print(f"Updated activeFoodIndex: {self.activeFoodIndex}")
+                                        await self.sendFoodIndex()
+
                                 else:
                                     print("Received message does not contain a valid pair of numbers")
                             except ValueError:
                                 print(f"Invalid data received: {received_message}")
-                        await asyncio.sleep(0.1)
+                            
+                        await asyncio.sleep(0) # Must be 0 so that received message buffer is always immediately
                 else:
                     print(f"Failed to connect to {self.BLUETOOTH_DEVICE_NAME}")
                     await asyncio.sleep(1)
             except Exception as e:
                 print(f"An error occurred: {e}")
-                listen_device.disconnect()
+                # listen_device.disconnect()
                 print("Attempting to reconnect...")
                 await asyncio.sleep(1)
+    
+    async def sendFoodIndex(self):
+        recipient_mac = b'\xff\xff\xff\xff\xff\xff'
+        self.networking.aen.send(recipient_mac, self.activeFoodIndex)
      
     async def main(self):
         await asyncio.gather(
@@ -117,8 +127,6 @@ class AprilTagMusicController:
             self.checkButtons()
         )
 
-
-        
-        
+# Create instance of controller class and run code
 controller = AprilTagMusicController()
 asyncio.run(controller.main())
