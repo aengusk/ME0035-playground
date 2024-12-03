cc9f5525fab9b97c4babb82cd85f3fca3a85bb61
aengusk <116749459+aengusk@users.noreply.github.com>
2024-12-03 11:54:32 -0500

    added esp_minimal_now and differentiated esp_minimal_nick

---
+++ woodchip_kitchen/esp_minimal_now.py
@@ -0,0 +1,20 @@
+from now import Now
+
+def my_callback(msg, mac):
+    print(mac, msg)
+    n.publish(msg, mac)
+
+n = Now(my_callback)
+n.connect()
+print(n.wifi.config('mac'))
+
+try:
+    while True:
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Interrupted! Cleaning up...")
+
+finally:
+    # Ensure interfaces are deactivated on exit
+    n.close()
