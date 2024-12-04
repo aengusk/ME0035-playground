eb91a3bef145ceba4eb19d88290d59f493128252
aengusk <116749459+aengusk@users.noreply.github.com>
2024-12-04 01:52:01 -0500

    final version of esp_minimal_now for presentation

--- woodchip_kitchen/esp_minimal_now.py
+++ woodchip_kitchen/esp_minimal_now.py
@@ -5,8 +5,33 @@ from BLE_Minimal import Yell # type: ignore
 yeller = Yell()
 
 def my_callback(msg, mac):
-    print(mac, msg)
-    n.publish(msg, mac)
+    if isinstance(mac, str):
+        if mac == 'spillover':
+            print('spillover')
+    print(mac)
+    msg = msg.decode()
+    print(msg)
+
+    if msg == '0':
+        print('0, burger')
+        yelltext = 'k' + msg
+        print('about to yell {}'.format(yelltext))
+        yeller.advertise(yelltext)
+    elif msg == '1':
+        print('1, smoothie')
+        yelltext = 'k' + msg
+        print('about to yell {}'.format(yelltext))
+        yeller.advertise(yelltext)
+    elif msg == '2':
+        print('2, ramen')
+        yelltext = 'k' + msg
+        print('about to yell {}'.format(yelltext))
+        yeller.advertise(yelltext)
+    else:
+        print('msg received was not interpreted: {}'.format(msg))
+
+
+
 
 n = Now(my_callback)
 n.connect()
@@ -14,7 +39,9 @@ print(n.wifi.config('mac'))
 
 try:
     while True:
-        time.sleep(1)
+        for _ in range(10):
+            time.sleep(1)
+        my_callback('0'.encode(), 'spillover')
 
 except KeyboardInterrupt:
     print("Interrupted! Cleaning up...")
